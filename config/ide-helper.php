b41dc3e1d5b6852cff739ec160581d1e73cec9d3
tak-solder <tak.solder@gmail.com>
2024-05-18 13:21:55 +0900

    edit model config

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'write_model_magic_where' => true,
+    'write_model_magic_where' => false,
 
     /*
     |--------------------------------------------------------------------------
