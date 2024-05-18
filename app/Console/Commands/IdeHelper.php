32815e095ec1912fb5a8b1f25588f63d168f6a19
tak-solder <tak.solder@gmail.com>
2024-05-18 13:02:31 +0900

    ide-helper:run command

---
+++ app/Console/Commands/IdeHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class IdeHelper extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ide-helper:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'run ide-helper:generate and ide-helper:meta if the environment is local';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (app()->environment('local')) {
+            $this->call('ide-helper:generate');
+            $this->call('ide-helper:meta');
+        }
+    }
+}
